ee1f934ad0622765639fdb3dbdccefa08f577180
DarkPhoenix6853 <caleb.klein@hotmail.com>
2020-11-13 18:26:00 +1000

    help update

--- SquadBot.js
+++ SquadBot.js
@@ -202,10 +202,10 @@ function help(message, perms) {
 
   const helpText = `
 Help for SquadBot: 
-**To create a squad**, use __${baseConfig.prefix}host__ followed by the message you want to display. 
-e.g. __${baseConfig.prefix}host lith survival__
-If you want to start the squad with 2 or 3 people (instead of just yourself), put the number of users before the host message
-e.g. you + 1 friend = __${baseConfig.prefix}host 2 k-drive racing__
+**To create a squad**, use __${baseConfig.prefix}host__ followed by the message you want to display. Capacity is optional. 
+e.g. __${baseConfig.prefix}host lith survival__ or __${baseConfig.prefix}host lith survival 1/4__
+If you want to start the squad with 2 or 3 people (instead of just yourself), put the number of users at the end of the message
+e.g. you + 1 friend = __${baseConfig.prefix}host k-drive racing 2/4__
 
 **To join a squad** just click the ✅ on someone else's host message
 
